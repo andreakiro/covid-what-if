68ef3ce6f958f4c07cc5e402a70eb5bf0ed7db33
apinto <andrea.pinto@epfl.ch>
2020-09-20 15:43:56 +0200

    Refactor repo

---
+++ frontend/src/components/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from './Navbar.js'
+import Footer from './Footer.js'
+
+export default function Layout() {
+    return (
+        <div>
+            <Nav/>
+            <Footer/>
+        </div>
+    );
+}
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,13 @@
+// export default function Nav() {
+//   return (
+//     <div className="px-4 md:px-8 lg:px-32 bg-main-600">
+//       <nav className="flex items-center justify-between py-2">
+//         <div className="flex items-center lg:w-1/2">
+//           <div className="flex items-center">
+//           <div className="hidden mx-4 sm:block">What if...?</div>
+//           </div>
+//         </div>
+//       </nav>
+//     </div>
+//   );
+// }
