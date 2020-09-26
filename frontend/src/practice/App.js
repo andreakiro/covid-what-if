1043807cd3ca8cecb1931b00be64353625ee3eb0
apinto <andrea.pinto@epfl.ch>
2020-09-26 22:52:34 +0200

    Update README

--- frontend/src/practice/App.js
+++ frontend/src/practice/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import Graph from "./components/Graph";
 import Layout from "./components/Layout";
 import Strength from "./components/Strenght";
+import Box from "./components/Box"
 
 function App() {
   return (
     <>
       <Layout>
-        <Graph />
-        <Strength />
+        {/* <Graph /> */}
+        {/* <Strength /> */}
+        <Box />
       </Layout>
     </>
   );
