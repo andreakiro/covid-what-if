cd00f6d393d7ec09e06ec5454e00db2d1a992a1f
apinto <andrea.pinto@epfl.ch>
2020-10-18 01:11:01 +0200

    Reposition elements

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -5,8 +5,8 @@ import Inputs from "./main/Inputs";
 export default function Main() {
   return (
     <>
-      <div className="flex">
-        <div className="w-1/3">
+      <div className="flex w-full mt-12">
+        <div className="w-1/3 flex justify-center">
           <Inputs />
         </div>
         <div className="w-2/3">
