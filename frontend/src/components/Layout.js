452dbc47c733bf4439243dd092aef0e0f4392bd4
apinto <andrea.pinto@epfl.ch>
2020-10-17 20:14:26 +0200

    Rearrange layout structure

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Nav from './layout-wise/Navbar.js'
-import Footer from './layout-wise/Footer.js'
+import Nav from './layout/Navbar.js'
+import Footer from './layout/Footer.js'
 
 export default function Layout(props) {
     return (
