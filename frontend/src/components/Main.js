4fd7f2d571eeeaf7b3081fe86990f8e6d3225bb2
apinto <andrea.pinto@epfl.ch>
2020-10-18 00:48:47 +0200

    Display inputs and graph

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Graph from "./main/Graph";
-import Inputs from "./main/Inputs"
+import Inputs from "./main/Inputs";
 
 export default function Main() {
   return (
     <>
-    <Inputs/>
-    {/* <Graph/> */}
+      <div className="flex">
+        <div className="w-1/3">
+          <Inputs />
+        </div>
+        <div className="w-2/3">
+          <Graph />
+        </div>
+      </div>
     </>
   );
 }
