2bbbd7a53673e1943830dca076ddce23a8304092
apinto <andrea.pinto@epfl.ch>
2020-10-18 02:28:01 +0200

    Add color when clicking download button

--- frontend/src/components/main/Inputs.js
+++ frontend/src/components/main/Inputs.js
@@ -1,6 +1,6 @@
-import { set } from "d3";
 import React, { useState } from "react";
 import Dropdown from "../subs/Dropdown";
+import { timeout } from "../../Utility.js";
 
 function Label({ text }) {
   return (
@@ -14,13 +14,23 @@ function Label({ text }) {
   );
 }
 
-function InputButton({ text, lock }) {
+function InputButton({ text, lock, activeAnimation }) {
+  let [active, setActive] = useState(false);
   return (
     <div>
       <span class="rounded-md shadow-sm">
         <button
           disabled={lock !== null}
-          class={`inline-flex justify-center w-full rounded-md border border-gray-500 px-4 py-2 bg-white text-sm leading-5 font-medium text-gray-700 ${
+          onClick={async () => {
+            if (activeAnimation) {
+              setActive(true);
+              await timeout(5000);
+              setActive(false);
+            }
+          }}
+          class={`inline-flex justify-center w-full rounded-md border border-${
+            active ? "green" : "gray"
+          }-500 px-4 py-2 bg-white text-sm leading-5 font-medium text-gray-700 ${
             lock !== null
               ? "cursor-default"
               : "hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition ease-in-out duration-150"
@@ -75,8 +85,8 @@ export default function Inputs(props) {
         />
       </div>
 
-      <InputButton text="Demographics" lock={lock} />
-      <InputButton text="Download" lock={lock} />
+      <InputButton text="Demographics" lock={lock} activeAnimation={false} />
+      <InputButton text="Download" lock={lock} activeAnimation={true} />
     </div>
   );
 }
