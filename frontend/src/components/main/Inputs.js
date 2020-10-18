a9f4455e9ee58347b0d329c7d163290b81e8e657
apinto <andrea.pinto@epfl.ch>
2020-10-18 02:11:04 +0200

    Handle dropdown switch

--- frontend/src/components/main/Inputs.js
+++ frontend/src/components/main/Inputs.js
@@ -1,14 +1,17 @@
+import { set } from "d3";
 import React, { useState } from "react";
 import Dropdown from "../subs/Dropdown";
 
 function Label({ text }) {
-  return <div>
-  <span class="rounded-md shadow-sm">
-    <p class="inline-flex justify-center w-full rounded-md border border-gray-500 px-4 py-2 bg-white text-sm leading-5 font-medium text-gray-700">
-      {text}
-    </p>
-  </span>
-</div>;
+  return (
+    <div>
+      <span class="rounded-md shadow-sm">
+        <p class="inline-flex justify-center w-full rounded-md border border-gray-500 px-4 py-2 bg-white text-sm leading-5 font-medium text-gray-700">
+          {text}
+        </p>
+      </span>
+    </div>
+  );
 }
 
 function InputButton({ text, lock }) {
@@ -30,54 +33,50 @@ function InputButton({ text, lock }) {
   );
 }
 
+function InputDropdown({ text, items, lock, setLock }) {
+  return (
+    <Dropdown
+      textselector={text}
+      items={items}
+      open={lock === text}
+      onOpen={() => setLock(text)}
+      onClose={() => setLock(null)}
+    />
+  );
+}
+
 export default function Inputs(props) {
-  let [using, setUsing] = useState(null);
+  let [lock, setLock] = useState(null);
   return (
     <div className="flex flex-col space-y-4 w-32">
-      
       <div className="flex flex-col space-y-2 w-32">
         <Label text="Country" />
-        <Dropdown
-          textselector="Select"
+        <InputDropdown
+          text="Select"
           items={["Global", "Switzerland", "Italy"]}
-          open={using === "Selector"}
-          onOpen={() => {
-            if (using === null) {
-              setUsing("Selector");
-            }
-          }}
-          onClose={() => setUsing(null)}
+          lock={lock}
+          setLock={setLock}
         />
       </div>
 
       <div className="flex flex-col space-y-2 w-32">
         <Label text="Time frame" />
-        <Dropdown
-          textselector="From"
+        <InputDropdown
+          text="From"
           items={["January", "February", "March"]}
-          open={using === "From"}
-          onOpen={() => {
-            if (using === null) {
-              setUsing("From");
-            }
-          }}
-          onClose={() => setUsing(null)}
+          lock={lock}
+          setLock={setLock}
         />
-        <Dropdown
-          textselector="Until"
+        <InputDropdown
+          text="Until"
           items={["January", "February", "March"]}
-          open={using === "Until"}
-          onOpen={() => {
-            if (using === null) {
-              setUsing("Until");
-            }
-          }}
-          onClose={() => setUsing(null)}
+          lock={lock}
+          setLock={setLock}
         />
       </div>
 
-      <InputButton text="Demographics" lock={using} />
-      <InputButton text="Download" lock={using} />
+      <InputButton text="Demographics" lock={lock} />
+      <InputButton text="Download" lock={lock} />
     </div>
   );
 }
