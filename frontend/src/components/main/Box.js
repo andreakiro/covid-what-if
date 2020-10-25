2340e7add8db37e1b69d7fa3890a35289654dece
apinto <andrea.pinto@epfl.ch>
2020-10-25 19:59:17 +0100

    Update accordingly to parameters provider

--- frontend/src/components/main/Box.js
+++ frontend/src/components/main/Box.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useParameters } from "../ParameterProvider";
 
 function Label({ text }) {
   return (
@@ -11,53 +12,56 @@ function Label({ text }) {
   );
 }
 
-export default function Box({ boxes, height }) {
+function BoxButton({ text, level, cycle }) {
+  return (
+    <button
+      onClick={() => cycle()}
+      class={`inline-flex justify-center ${
+        level == 0
+          ? "bg-white"
+          : level == 1
+          ? "bg-yellow-500"
+          : level == 2
+          ? "bg-orange-500"
+          : "bg-red-600"
+      } px-4 py-2 text-sm leading-5 font-medium ${
+        level == 0 ? "text-gray-700" : "text-white"
+      } ${
+        level == 0 ? "text-grey-700" : "text-white"
+      } w-full rounded-md border ${
+        level == 0 ? "border-gray-500" : ""
+      } px-4 py-2 text-sm leading-5 font-medium hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition ease-in-out duration-150`}
+    >
+      {text}
+    </button>
+  );
+}
+
+export default function Box({ boxes }) {
+  let { height, level, cycle } = useParameters();
   let [row, setRow] = useState(Array.from({ length: boxes }, (_, i) => i + 1));
   let [width, setWidth] = useState(
     Array.from({ length: height }, (_, i) => i + 1)
   );
-  let [level, setLevel] = useState(0);
 
-  let cycle = () => {
-    console.log(level);
-    if (level == 3) {
-      setLevel(0);
-    } else {
-      setLevel((level += 1));
-    }
-  };
   return (
-    <div class="flex flex-col justify-center space-y-2">
-      <Label text="Each box represent 1/8 of the time frame" />
+    <div class="flex flex-col items-center space-y-2">
+      <div class="text-left w-4/5">
+        <Label text="Each box represent 1/8 of the time frame" />
+      </div>
       <span class="inline-flex justify-center w-4/5 rounded-md border border-gray-600 px-4 py-2 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition ease-in-out duration-150">
         <div className="flex flex-col space-y-2 w-full">
-          {width.map(() => {
+          {width.map((y) => {
             return (
               <div class="flex justify-center w-full space-x-2">
-                {row.map((i) => {
+                {row.map((x) => {
+                  let i = ((y - 1) << 3) + (x - 1);
                   return (
-                    <button
-                      onClick={() => cycle()}
-                      class={`inline-flex justify-center bg-${
-                        level == 0
-                          ? "white"
-                          : level == 1
-                          ? "yellow-500"
-                          : level == 2
-                          ? "orange-500"
-                          : "red-600"
-                      } px-4 py-2 text-sm leading-5 font-medium text-${
-                        level == 0 ? "gray-700" : "white"
-                      } w-full rounded-md border border-${
-                        level == 0 ? "gray-500" : ""
-                      } px-4 py-2 text-sm leading-5 font-medium hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition ease-in-out duration-150`}
-                    >
-                      <p
-                        className={`text-${level == 0 ? "grey-700" : "white"}`}
-                      >
-                        {i}
-                      </p>
-                    </button>
+                    <BoxButton
+                      text={x}
+                      level={level[i]}
+                      cycle={() => cycle(i)}
+                    />
                   );
                 })}
               </div>
@@ -65,7 +69,9 @@ export default function Box({ boxes, height }) {
           })}
         </div>
       </span>
-      <Label text="Click on a box to increase policy level" />
+      <div class="text-right w-4/5">
+        <Label text="Click on a box to increase policy level" />
+      </div>
     </div>
   );
 }
