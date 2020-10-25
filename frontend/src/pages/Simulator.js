0ba5328d95aaf8e3ad67b745dc4dfab9b0b5cb85
apinto <andrea.pinto@epfl.ch>
2020-10-26 00:47:59 +0100

    Arrange simulator components on the page

--- frontend/src/pages/Simulator.js
+++ frontend/src/pages/Simulator.js
@@ -4,21 +4,42 @@ import Inputs from "../components/main/Inputs";
 import Graph from "../components/main/Graph";
 import Policies from "../components/main/Policies";
 import ParameterProvider from "../components/ParameterProvider";
+import * as d3 from "d3";
 
 export default function Simulator() {
+  let [bigdata, setBigdata] = React.useState([]);
+  let bigdata2 = [
+    ["2013-04-28", 2],
+    ["2013-04-29", 1],
+    ["2013-05-01", 3],
+    ["2013-05-02", 0.5],
+    ["2013-05-03", 3.5],
+  ].map((x) => ({ date: d3.timeParse("%Y-%m-%d")(x[0]), value: x[1] }));
+
+  let onClick = () => {
+    const newelement = { date: new Date(), value: Math.random() * 4 };
+    setBigdata((bigdata) => [...bigdata, newelement]);
+  };
+
   return (
     <ParameterProvider>
-      <div className="flex w-full mt-12">
-        <div className="w-1/3 flex justify-center">
-          <div class="flex-col space-y-6">
+      <button onClick={onClick}>Add</button>
+      <button onClick={() => setBigdata([])}>Reset</button>
+      <div className="flex flex-col w-10/12 mt-8 mb-8 divide-y divide-gray-400">
+        <div className="flex pb-4">
+          <div className="w-1/3 flex justify-center">
             <Inputs />
-            <Policies />
+          </div>
+          <div className="w-2/3 flex justify-center">
+            <Graph bigdata={bigdata} />
           </div>
         </div>
-        <div className="w-2/3">
-          <div class="flex-col space-y-6">
-            {/* <Graph /> */}
-            <Box boxes={8} height={4} />
+        <div className="flex pt-8">
+          <div className="w-1/3 flex justify-center">
+            <Policies />
+          </div>
+          <div className="w-2/3 flex justify-center">
+            <Box boxes={8} />
           </div>
         </div>
       </div>
