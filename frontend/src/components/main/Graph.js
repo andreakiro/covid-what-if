edd91a0cb93984b85b9f54cce2626ccd36ad2191
apinto <andrea.pinto@epfl.ch>
2020-10-26 00:45:05 +0100

    Make graph cool & functional

--- frontend/src/components/main/Graph.js
+++ frontend/src/components/main/Graph.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as d3 from "d3";
 
-export default function Graph() {
+export default function Graph({ bigdata }) {
   const ref = React.useRef(null);
 
   const margin = {
@@ -11,96 +11,60 @@ export default function Graph() {
     left: 60,
   };
 
-  const width = 460 - margin.left - margin.right;
-  const height = 400 - margin.top - margin.bottom;
-
   React.useEffect(() => {
     if (!ref.current) return;
 
-    const svg = d3
-      .select(ref.current)
+    const width = ref.current.clientWidth - margin.left - margin.right;
+    const height = ref.current.clientHeight - margin.top - margin.bottom;
+
+    const container = d3.select(ref.current);
+    container.selectAll("*").remove();
+    const svg = container
       .append("g")
       .attr("transform", `translate(${margin.left}, ${margin.top})`);
-      
-    d3.csv(
-      "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv",
-      (d) => ({ date: d3.timeParse("%Y-%m-%d")(d.date), value: d.value })).then(
-      (data) => {
-        console.log(data);
-          
-        // x axis
-        const x = d3
-          .scaleTime()
-          .domain(d3.extent(data, (d) => d.date))
-          .range([0, width]);
 
-        svg
-          .append("g")
-          .attr("transform", `translate(0, ${height})`)
-          .call(d3.axisBottom(x));
+    const data = bigdata;
 
-        // y axis
-        const y = d3
-          .scaleLinear()
-          .domain([0, d3.max(data, (d) => +d.value)])
-          .range([height, 0]);
+    // x axis
+    const x = d3
+      .scaleTime()
+      .domain(d3.extent(data, (d) => d.date))
+      .range([0, width]);
 
-        svg.append("g").call(d3.axisLeft(y));
+    svg
+      .append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(d3.axisBottom(x));
 
-        // add the  line
+    // y axis
+    const y = d3.scaleLinear().domain([0, 4]).range([height, 0]);
 
-        const line = d3
-          .line()
-          .x((d) => x(d.date))
-          .y((d) => y(d.value));
+    svg.append("g").call(d3.axisLeft(y));
 
-        svg
-          .append("path")
-          .datum(data)
-          .attr("fill", "none")
-          .attr("stroke", "steelblue")
-          .attr("stroke-width", 1.5)
-          .attr("d", line);
-      }
-    );
-  }, [ref]);
+    // add the  line
+    const line = d3
+      .line()
+      .x((d) => x(d.date))
+      .y((d) => y(d.value));
 
-  return (
-    <svg
-      width={width + margin.left + margin.right}
-      height={height + margin.top + margin.bottom}
-      ref={ref}
-    ></svg>
-  );
-}
+    svg
+      .append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "green")
+      .attr("stroke-width", 1.5)
+      .attr("d", line);
 
-export function Graph1() {
-  const ref = React.useRef(null);
-  React.useEffect(() => {
-    if (!ref.current) return;
-    const jsonCircles = [
-      {
-        x_axis: 30,
-        y_axis: 30,
-        radius: 20,
-        color: "green",
-      },
-    ];
+    svg
+      .append("line")
+      .attr("x1", 0)
+      .attr("y1", y(1))
+      .attr("x2", width)
+      .attr("y2", y(1))
+      .attr("stroke-width", 1)
+      .attr("stroke", "orange")
+      .style("stroke-dasharray", "5,5");
+  }, [ref, bigdata]);
 
-    const svgContainer = d3.select(ref.current);
-
-    const circles = svgContainer
-      .selectAll("circle")
-      .data(jsonCircles)
-      .enter()
-      .append("circle");
-
-    const circleAttributes = circles
-      .attr("cx", (d) => d.x_axis)
-      .attr("cy", (d) => d.y_axis)
-      .attr("r", (d) => d.radius)
-      .attr("fill", (d) => d.color);
-  }, [ref]);
-
-  return <svg width="200" height="200" ref={ref}></svg>;
+  return <svg ref={ref} className="w-full h-auto"></svg>;
 }
