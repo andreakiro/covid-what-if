30df80ca5e9b48e531aba3776d76abe89b99b657
apinto <andrea.pinto@epfl.ch>
2020-10-26 14:28:59 +0100

    Milestone 1.0

--- frontend/src/pages/Simulator.js
+++ frontend/src/pages/Simulator.js
@@ -8,13 +8,6 @@ import * as d3 from "d3";
 
 export default function Simulator() {
   let [bigdata, setBigdata] = React.useState([]);
-  let bigdata2 = [
-    ["2013-04-28", 2],
-    ["2013-04-29", 1],
-    ["2013-05-01", 3],
-    ["2013-05-02", 0.5],
-    ["2013-05-03", 3.5],
-  ].map((x) => ({ date: d3.timeParse("%Y-%m-%d")(x[0]), value: x[1] }));
 
   let onClick = () => {
     const newelement = { date: new Date(), value: Math.random() * 4 };
