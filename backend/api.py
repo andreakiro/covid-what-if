806406669e5ee2973704e95027a58e5937c98ff7
apinto <andrea.pinto@epfl.ch>
2020-11-09 23:10:19 +0100

    API version 1.0

--- backend/api.py
+++ backend/api.py
@@ -5,6 +5,26 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route('/', methods=['POST'])
+def send():
+    # Get request
+    body = request.json
+    
+    # Retrieve parameters
+    country = body['country']
+    tfFrom = body['time-frame']['from'] # Format 'DD.MM.YYYY'
+    tfUntil = body['time-frame']['until'] # Format 'DD.MM.YYYY'
+    policies = body['policies']
+
+    # Call the model
+    r0 = getR0(getCode(country), tfFrom, tfUntil, policies)
+    
+    return {
+        "response": {
+            "r0": r0
+        }
+    }
+
+@app.route('/', methods=['POST'])
 def hello_world():
     body = request.json
     a = int(body['a'])
--- frontend/src/API.js
+++ frontend/src/API.js
@@ -7,3 +7,43 @@ async function send(params) {
   let result = await fetch("http://localhost:5000", request).json();
   return result;
 }
+
+async function request(params, model = "hybrid") {
+  if (! validInput(params)) {
+    console.log("Given parameters have wrong format.");
+    return null;
+  }
+
+  let content = {
+    "model-id": model,
+    "args": params
+  };
+
+  let request = {
+    method: "POST",
+    headers: { "Content-Type": "application/JSON" },
+    body: JSON.stringify(content),
+  };
+
+  let response = await fetch("http://localhost:5000", request).json();
+
+  return response;
+}
+
+// Utilities : format checks
+
+function validInput(params) {
+  if (! ('country' in params)) return false;
+  if (! ('time-frame' in params)) return false;
+  if (! ('time-frame.from' in params)) return false;
+  if (! ('time-frame.until' in params)) return false;
+  if (! ('policies' in params)) return false;
+  for (i = 1; i < 9; i ++)
+    if (! (('policies.c' + i) in params)) return false;
+  return true;
+}
+
+function validOutput(response) {
+  if (! ('r0' in response)) return false;
+  return true;
+}
