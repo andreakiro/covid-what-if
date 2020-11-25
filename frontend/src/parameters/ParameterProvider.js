88e7c0f67f62118a8b78e12006136211ea4d33df
apinto <andrea.pinto@epfl.ch>
2020-11-25 15:45:36 +0100

    First reducer usage : it will change the overall structure

--- frontend/src/parameters/ParameterProvider.js
+++ frontend/src/parameters/ParameterProvider.js
@@ -1,6 +1,7 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useState, useReducer } from "react";
 import { BoxParameters } from "./simulator/BoxParameters";
 import { InputsParameters } from "./simulator/InputsParameters";
+import { policyFromList } from "./util";
 
 const Context = createContext(null);
 
@@ -26,22 +27,44 @@ export function getParameters() {
   return parameters;
 }
 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'INIT UID': return {
+      ...state,
+      uid: action.uid
+    }
+  }
+}
+
 export default function ParameterProvider(props) {
   let boxParams = BoxParameters();
   let inputsParams = InputsParameters();
 
-  let parameters = {
+  let [policies] = useState(policyFromList(Array(8).fill([])));
+  let [uid, setUID] = useState(0);
+
+  let initialState = {
+    uid: 0,
+  };
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  let parameters2 = {
+    uid: uid,
     values: {
-      ...boxParams.values,
+      policies,
       ...inputsParams.values,
+      ...boxParams.values,
     },
     functions: {
       ...boxParams.functions,
       ...inputsParams.functions,
+      setUID,
     },
   };
 
+  let parameters = [state, dispatch];
+
   return (
-    <Context.Provider value={parameters}>{props.children}</Context.Provider>
+    <Context.Provider value={parameters2}>{props.children}</Context.Provider>
   );
 }
