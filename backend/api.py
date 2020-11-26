4b3d4e78c0ded978275f7a0a2a7a7f6cd42eadf6
apinto <andrea.pinto@epfl.ch>
2020-11-26 16:03:51 +0100

    API to fetch countries

--- backend/api.py
+++ backend/api.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS
 import secrets
 from modeling.unified import *
+from utils.countries import *
 import json
 import numpy as np
 
@@ -11,49 +12,45 @@ CORS(app)
 update_func = dict()
 
 def api_load(args):
-    print('Hey it seems you want to load some data, with this request :')
-    print(args)
     uid = args['uid']
-    country = args['country']
+    country_iso = getIso(args['country'])
     tfrom = args['tframe']['from']
     tuntil = args['tframe']['until']
-    _, sub_data, update = load(country, tfrom, tuntil)
+    _, sub_data, update = load(country_iso, tfrom, tuntil)
     policies, target, dates, pred = sub_data
     update_func[uid] = update
-    print('Here is the length of the dict : ' + str(len(update_func)))
-    body = {
+    return {
         "response": {
-            "r0": list(np.nan_to_num(pred)),
+            "pred": list(np.nan_to_num(pred)),
             "target": list(np.nan_to_num(target)),
             "dates": [str(x) for x in dates],
             "policies": {k: list(v) for k, v in policies.items()}
         }
     }
-    #print(json.dumps(body))
-    return body
 
 def api_update(args):
-    print('Hey it seems you want to update your params')
     uid = args['uid']
-    country = args['country']
+    country_iso = getIso(args['country'])
     tfrom = args['tframe']['from']
     tuntil = args['tframe']['until']
     policies = args['policies']
-    pred, target, _ = update_func[uid](country, tfrom, tuntil, policies)
+    pred, target, _ = update_func[uid](country_iso, tfrom, tuntil, policies)
     return {
         "response": {
-            "r0": list(np.nan_to_num(pred)),
+            "pred": list(np.nan_to_num(pred)),
             "target": list(np.nan_to_num(target))
         }
     }
 
 def api_init():
-    print('Hey lets init your app')
-    print('Current dict length : ' + str(len(update_func)))
     return {
-        "countries": ['United States', 'Italy', 'Switzerland', 'Bangladesh'],
+        "countries": getAvailableCountries(),
         "uid": secrets.token_urlsafe(8)
     }
+
+@app.route('/init', methods=['POST'])
+def init():
+    return {"hey": "hey"}
 
 @app.route('/', methods=['POST'])
 def api():
