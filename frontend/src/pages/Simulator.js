83571f654821013686c639b957ea18528443ff01
apinto <andrea.pinto@epfl.ch>
2020-12-03 18:01:33 +0100

    Hide lower part before initializing the country

--- frontend/src/pages/Simulator.js
+++ frontend/src/pages/Simulator.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { init } from "../API";
 import Box from "../components/simulator/Box";
 import GraphManager from "../components/simulator/GraphManager";
@@ -12,6 +12,7 @@ async function initSession(dispatch) {
     type: "initsession",
     uid: response.uid,
     countries: response.countries,
+    policynames: response.policies,
   });
 }
 
@@ -22,7 +23,7 @@ export default function Simulator() {
   }, [dispatch]);
   return (
     <>
-      <button
+      {/* <button
         onClick={() =>
           dispatch({
             type: "log",
@@ -31,7 +32,7 @@ export default function Simulator() {
         className="inline-flex justify-center w-20 rounded-md border border-gray-300 px-4 py-2 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition ease-in-out duration-150"
       >
         Log
-      </button>
+      </button> */}
       <div className="flex flex-col w-10/12 mt-8 mb-8 divide-y divide-gray-400">
         <div className="flex pb-4">
           <div className="w-1/3 flex justify-center">
@@ -41,7 +42,7 @@ export default function Simulator() {
             <GraphManager />
           </div>
         </div>
-        <div className="flex pt-8">
+        <div className={`flex pt-8 ${state.country === null ? "hidden" : ""}`}>
           <div className="w-1/3 flex justify-center">
             <Policies />
           </div>
